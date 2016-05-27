2afb832e24dc8ad3b5f83f5b531b54fbc7e8e48e
Bang Dao <bang.dao@eastagile.com>
2016-05-27 10:46:22 +0700

    [f] serve static file on heroku with whitenoise

--- root/settings/staging/heroku.py
+++ root/settings/staging/heroku.py
@@ -1,1 +1,6 @@
 from root.settings.base import *
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
