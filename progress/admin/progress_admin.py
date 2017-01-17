d35db4ac46962c9a407bf1944152847208452c60
Bang Dao <daotranbang@gmail.com>
2017-01-18 04:07:34 +0700

    render progress bar with current milestone listed

--- progress/admin/progress_admin.py
+++ progress/admin/progress_admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from progress.models import Progress
+from progress.models import Progress, Milestone
+
+
+class MilestoneInline(admin.TabularInline):
+    model = Milestone
 
 
 class ProgressAdmin(admin.ModelAdmin):
+    inlines = [
+        MilestoneInline,
+    ]
+
     def get_form(self, request, obj=None, **kwargs):
         form = super(ProgressAdmin, self).get_form(request, obj, **kwargs)
         form.base_fields['state'].queryset = form.base_fields['state'].queryset.filter(progress=obj)
---
+++ progress/templatetags/progress.py
@@ -0,0 +1,40 @@
+from django import template
+from django.db.models.aggregates import Sum
+
+from progress.models import Progress
+
+register = template.Library()
+
+
+def get_default_percentage(progress):
+    milestones_count = progress.milestones.all().count()
+    custom_percentages = progress.milestones.filter(percentage__gt=0).aggregate(Sum('percentage'))
+    custom_percentages_sum = custom_percentages['percentage__sum'] or 0
+    return (100 - custom_percentages_sum) / milestones_count
+
+@register.inclusion_tag('progress/progress.html')
+def progress(code_name):
+    progress = Progress.objects.filter(code_name=code_name).first()
+    if not progress:
+        return
+
+    default_percentage = get_default_percentage(progress)
+    finished_milestones = progress.milestones.finished()
+    started_milestones = progress.milestones.started()
+    unstarted_milestones = progress.milestones.unstarted()
+
+    finished_percentage = int(sum(x.percentage or default_percentage for x in finished_milestones))
+    started_percentage = int(sum(x.percentage or default_percentage for x in started_milestones))
+    unstarted_percentage = 100 - finished_percentage - started_percentage
+
+    return {
+        'progress': progress,
+
+        'finished_milestones': finished_milestones,
+        'started_milestones': started_milestones,
+        'unstarted_milestones': unstarted_milestones,
+
+        'finished_percentage': finished_percentage,
+        'started_percentage': started_percentage,
+        'unstarted_percentage': unstarted_percentage,
+    }
